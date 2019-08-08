0a85758571552be95315016d592b964fa7c1e8bd
Simon THOBY <git@nightmared.fr>
2019-08-08 10:28:33 +0200

    fix the basic math operations

--- config.py
+++ config.py
@@ -1,19 +1,26 @@
 from lib import *
 
+EPSILON = 1e-9
+
 CAMERAS = [
-    Camera(Vec3D.Cylindrical(0.6/math.sqrt(3), 0, 3.3)),
-    Camera(Vec3D.Cylindrical(0.6/math.sqrt(3), 2*math.pi/3, 3.3)),
-    Camera(Vec3D.Cylindrical(0.6/math.sqrt(3), 4*math.pi/3, 3.3))
+    Camera(1, Vec3D.Cylindrical(0.6/math.sqrt(3), 0, 3.3), 1.7),
+    Camera(2, Vec3D.Cylindrical(0.6/math.sqrt(3), 2*math.pi/3, 3.3), 2.5),
+    Camera(3, Vec3D.Cylindrical(0.6/math.sqrt(3), 4*math.pi/3, 3.3), 3.4)
+]
+
+CAMERAS_DELTAS = [
+    Vec3D(0.3, 0.16, 0),
+    Vec3D(0.07, 0.41, 0),
+    Vec3D(-0.06, 0.05, 0)
 ]
 
 FIDUCIALS = [
-    Fiducial(CAMERAS[0], Vec3D(0.15, 0.37, 0)),
-    Fiducial(CAMERAS[1], Vec3D(0.15, 0.37, 0).rotate_around_origin(2*math.pi/3)),
-    Fiducial(CAMERAS[0], Vec3D(0.06, 0.18, 0)),
-    Fiducial(CAMERAS[1], Vec3D(0.06, 0.18, 0).rotate_around_origin(2*math.pi/3))
+    Fiducial(CAMERAS[0], Vec3D(0.15, 0.37, 0)+CAMERAS_DELTAS[0]),
+    Fiducial(CAMERAS[1], (Vec3D(0.15, 0.37, 0)+CAMERAS_DELTAS[1]).rotate_around_origin(2*math.pi/3, origin=CAMERAS_DELTAS[1])),
+    Fiducial(CAMERAS[0], Vec3D(0.06, 0.18, 0)+CAMERAS_DELTAS[0]),
+    Fiducial(CAMERAS[1], (Vec3D(0.06, 0.18, 0)+CAMERAS_DELTAS[1]).rotate_around_origin(2*math.pi/3, origin=CAMERAS_DELTAS[1]))
 ]
 
 REFERENCES = [
-    Reference(FIDUCIALS[0], FIDUCIALS[1]),
-    Reference(FIDUCIALS[2], FIDUCIALS[3])
+    Reference.new(Vec3D(0.15, 0.37, 0), (FIDUCIALS[0], FIDUCIALS[1]), (FIDUCIALS[2], FIDUCIALS[3]))
 ]
--- lib.py
+++ lib.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import math
 from typing import Type, Tuple
+import config
 
 class Vec2D:
     def __init__(self, x, y):
@@ -31,6 +32,15 @@ class Vec3D:
     def __add__(self, other: Type[Vec3D]):
         return Vec3D(self.x+other.x, self.y+other.y, self.z+other.z)
 
+    def __neg__(self):
+        return Vec3D(-self.x, -self.y, -self.z)
+
+    def __sub__(self, other: Type[Vec3D]):
+        return self+-other
+
+    def __str__(self):
+        return f"""Vec3D({self.x}, {self.y}, {self.z})"""
+
     def norm_xy(self):
         return math.sqrt((self.x**2)+(self.y**2))
 
@@ -40,13 +50,26 @@ class Vec3D:
     def distance(self, other: Type[Vec3D]):
         (self-other).norm()
 
-    def rotate_around_origin(self, theta, phi = None) -> Vec3D:
-        if phi is None:
+    def rotate_around_origin(self, theta, phi = None, origin = None) -> Vec3D:
+        obj = self
+        if origin is not None:
+            obj = self-origin
+
+        if phi is None or phi == 0.0 or phi == -0.0:
             # Plane rotation
-            return Vec3D(self.x*math.cos(theta)-self.y*math.sin(theta), self.x*math.sin(theta)+self.y*math.cos(theta), self.z)
+            res = Vec3D(obj.x*math.cos(theta)-obj.y*math.sin(theta), obj.x*math.sin(theta)+obj.y*math.cos(theta), obj.z)
         else:
-            r = self.norm()
-            return Vec3D(r*math.cos(theta)*math.cos(phi), r*math.sin(theta)*math.cos(phi), r*math.sin(phi))
+            r, theta_cur, phi_cur = obj.get_coordinates_spherical()
+            res = Vec3D(r*math.cos(theta+theta_cur)*math.sin(phi+phi_cur), r*math.sin(theta+theta_cur)*math.sin(phi+phi_cur), r*math.cos(phi+phi_cur))
+
+        if origin is not None:
+            return res+origin
+        else:
+            return res
+
+    # Return the coordinates in the cartesian coordinates system
+    def get_coordinates(self) -> Tuple[float, float, float]:
+        return (self.x, self.y, self.z)
 
     # Return the coordinates in the spherical coordinates system
     def get_coordinates_spherical(self) -> Tuple[float, float, float]:
@@ -66,10 +89,10 @@ class Vec3D:
         theta_res = theta1-theta2
         # minimize the angle
         if abs(theta_res) > abs(2*math.pi-theta_res):
-            theta_res = 2*math.pi-theta_res
+            theta_res = theta_res-2*math.pi
         phi_res = phi1-phi2
         if abs(phi_res) > abs(2*math.pi-phi_res):
-            phi_res = 2*math.pi-phi_res
+            phi_res = phi_res-2*math.pi
         return (theta_res, phi_res)
 
 
@@ -79,19 +102,53 @@ class BubbleChamber:
 
 class Camera:
     # We assume the camera is always oriented towards the origin (0, 0, 0) and we specify its position with regard to this origin
-    def __init__(self, position: Type[Vec3D]):
+    def __init__(self, number: int, position: Type[Vec3D], scaling: float):
+        self.number = number
         self.position = position
+        self.scaling = scaling
 
 
 class Reference:
-    # Create a new reference from two views of the same fiducial
-    def __init__(self, p1: Type[Fiducial], p2: Type[Fiducial]):
-        if p1.camera == p2.camera:
+    def __init__(self, cam1, offset_cam1, cam2, offset_cam2):
+            self.cam1 = cam1
+            self.offset_cam1 = offset_cam1
+            self.cam2 = cam2
+            self.offset_cam2 = offset_cam2
+
+    # Create a new reference from two fiducials and the real position of the first one, given two views per fiducial
+    @classmethod
+    def new(cls, fiducial1_real_pos: Vec3D, fiducial1: Tuple[Type[Fiducial], Type[Fiducial]], fiducial2: Tuple[Type[Fiducial], Type[Fiducial]]) -> Type[Reference]:
+        if fiducial1[0].camera.number == fiducial1[1].camera.number or fiducial2[0].camera.number == fiducial2[1].camera.number:
             raise AssertionError("The two measurements come from the same camera !")
+        # The two cameras used must be identical in the two pairs
+        if fiducial1[0].camera.number != fiducial2[0].camera.number or fiducial1[1].camera.number != fiducial2[1].camera.number:
+            raise AssertionError("The cameras used must be identical for the two fiducials !")
 
         # Try to compute the rotation between the two cameras, relative to the origin
-        (theta, phi) = p1.position.get_angle_with_origin(p2.position)
-        print((theta, phi))
+        (theta_cam, phi_cam) = fiducial1[0].camera.position.get_angle_with_origin(fiducial1[1].camera.position)
+
+
+        ### Try to guess the position of the origin
+
+        # vector between the two fiducials
+        delta_x_fid, delta_y_fid, _ = (fiducial2[0].position-fiducial1[0].position).get_coordinates()
+        # Sanity check: ensure this is the same fiducial by comparing the vector from the first to the second fiducial in both camera views
+        delta_x_fid_cam1, delta_y_fid_cam1, _ = (fiducial2[1].position-fiducial1[1].position).rotate_around_origin(theta_cam, phi_cam).get_coordinates()
+        if abs(delta_x_fid_cam1-delta_x_fid) > config.EPSILON or abs(delta_y_fid_cam1-delta_y_fid) > config.EPSILON:
+            raise AssertionError("You must use the same fiducials when trying to establish a reference")
+
+        # Offset between the real origin and the origin in the coordinates system of camera1
+        cam1 = fiducial1[0].camera.number
+        offset_cam1 = fiducial1[0].position-fiducial1_real_pos
+        # Offset between the real origin and the origin in the coordinates system of camera2
+        cam2 = fiducial1[1].camera.number
+        offset_cam2 = fiducial1[1].position-fiducial1_real_pos.rotate_around_origin(-theta_cam, -phi_cam)
+
+        return cls(cam1, offset_cam1, cam2, offset_cam2)
+
+                # Return the reference with cameras inverted
+        def __neg__(self):
+            return Vec3D(self.cam2, self.offset_cam2, self.cam1, self.offset_cam1)
 
 
 class Measurement:
